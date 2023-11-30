1b6ccc7be98a7a10a02fd9dd8b22a23af69ac056
simeunseo <rkdcjfehowl@naver.com>
2023-11-30 22:34:46 +0900

    feat: 기본 상태

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,11 @@ const Home = ({ posts }) => {
       <HeadInfo />
       <h1>심은서의 심화 스터디 실습 - 심심스실</h1>
       <ul>
-        {posts.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
-        })}
+        <li>title1</li>
+        <li>title2</li>
       </ul>
     </div>
   );
 };
 
 export default Home;
-
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_start=0&_end=30`
-  );
-  const posts = await res.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
